5bf0d84dfb492cd5f541e9dce000dfa9f2fcef4a
yoshi3939 <irieyoshimi43@gmail.com>
2021-07-16 21:07:55 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :last_name
-    validates :first_name
-  end
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
 
-  validates :nickname, presence: true
-  validates :birth_date, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    with_options format: { with: /\A[ァ-ヶー]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
