0583f326ced64ab88ecc01c84a3c388a50d82c2b
yoshi3939 <irieyoshimi43@gmail.com>
2021-07-21 15:19:29 +0900

    itemモデルの単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name  { Faker::Name.name }
+    description { Faker::Lorem.sentence }
+    category_id { '1' }
+    item_status_id { '1' }
+    delivery_charge_id { '1' }
+    prefecture_id { '1' }
+    delivery_days_id { '1' }
+    price { '500' }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
