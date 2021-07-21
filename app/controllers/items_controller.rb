9e8f98a3aa88ff76bce8a7742e0b428d0f78bcb7
yoshi3939 <irieyoshimi43@gmail.com>
2021-07-21 09:46:20 +0900

    商品情報をデータベースに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, expect: :index
+
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :category_id, :item_status_id, :delivery_charge_id, :prefecture_id, :delivery_days_id, :price).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+  end
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_days
+
+  validates :category_id, :item_status_id, :delivery_charge_id, :prefecture_id, :delivery_days_id, numericality: { other_than: 0 , message: "can't be blank" }
+  validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+end
