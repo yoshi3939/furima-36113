8b4c509c0bdbd837bb6c4e9ecc1ed28de32540f5
yoshi3939 <irieyoshimi43@gmail.com>
2021-07-26 14:43:10 +0900

    商品購入機能　修正5

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -31,7 +31,7 @@ class RecordsController < ApplicationController
   end
 
   def confirm_current_user
-    redirect_to root_path if (current_user.id != @item.user_id) || @item.record.present?
+    redirect_to root_path if (current_user.id == @item.user_id) || @item.record.present?
   end
 
   def pay_item
