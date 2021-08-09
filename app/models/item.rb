616fc7ea77d743bad59fd3bcdff43b6a64fb1347
yoshi3939 <irieyoshimi43@gmail.com>
2021-08-09 20:50:13 +0900

    エラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
   belongs_to :delivery_days
 
   validates :category_id, :item_status_id, :delivery_charge_id, :prefecture_id, :delivery_days_id,
-            numericality: { other_than: 0, message: "can't be blank" }
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' },
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+            numericality: { other_than: 0, message: 'を選択してください' }
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角数字のみ入力してください' },
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は300~9,999,999の範囲で入力してください' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birth_date
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字のみ入力可です' }
 
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角のみ入力可です'} do
       validates :last_name
       validates :first_name
     end
 
-    with_options format: { with: /\A[ァ-ヶー]+\z/ } do
+    with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カナのみ入力可です' } do
       validates :last_name_kana
       validates :first_name_kana
     end
